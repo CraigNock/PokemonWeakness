e77632af1893915fde182ae38322b2f99c3c366f
Craig Nockels <cnockels@gmail.com>
2020-07-05 22:31:17 -0400

    test 7

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,4 +1,23 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -14,6 +33,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
+const path = __importStar(require("path"));
 const cors_1 = __importDefault(require("cors"));
 const request_promise_1 = __importDefault(require("request-promise"));
 const morgan_1 = __importDefault(require("morgan"));
@@ -77,6 +97,7 @@ const pokemonTypeHandler = (req, res) => __awaiter(void 0, void 0, void 0, funct
     }
     ;
 });
+app.use(express_1.default.static(path.join(__dirname, '../../client/build')));
 app.use(body_parser_1.default.json());
 app.use(cors_1.default());
 app.use(morgan_1.default('tiny'));
@@ -86,9 +107,9 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
-app.get('/', (req, res) => {
-    res.send('hi');
-});
 app.get('/pokemon/:name', pokemonTypeHandler);
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '../../client/build/index.html'));
+});
 app.listen(PORT, () => { console.log(`Listening on Porto ${PORT}`); });
 //# sourceMappingURL=index.js.map
